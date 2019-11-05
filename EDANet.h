347dcd26db4defbde79219497d2a7a5844c3f45f
ALE <aleronpower@hotmail.com>
2019-11-04 23:49:27 -0300

    Declaracion (y algunas ayuditas) del EDANet listo Favor de revisarlo y hacerlo menos mediocre (ese new q siempre devuelve true xD, al cabo q tambien hay de esos en mi parte del codigo :V )

--- EDANet.h
+++ EDANet.h
@@ -7,10 +7,16 @@
 class EDANet : public Subject {
 public:
 	EDANet();
-	~EDANet();
-	bool createNode(string IP, string port);
-	unsigned int getNodeAmount(void) { return nodes.size; };
+	~EDANet() { for (int i = 0; i < nodes.size(); i++) delete nodes[i]; }
+	bool createNode(const string& IP, const string& port, const string& ID) {
+		Node* newNode = new Node(ID, port, IP);
+		nodes.emplace_back(newNode);
+		notifyAllObservers();
+		return true;
+	}
+	unsigned int getNodeAmount(void) { return nodes.size(); };
+	Node* getNode(unsigned int nodePos) { Node* rta = nullptr; if (nodePos < nodes.size() && nodePos >= 0) rta = nodes[nodePos]; return rta; }
 
 private:
-	vector<Node> nodes;
+	vector<Node *> nodes;
 };
